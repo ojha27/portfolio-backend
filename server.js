6196020f59cabf133a38a4acd69de48c61fe3095
Amrita Ojha <amrita@topsinfosolutions.com>
2026-05-27 15:48:26 +0530

    Fix CORS for Vercel

--- server.js
+++ server.js
@@ -1,24 +1,30 @@
-const express = require('express')
-const cors = require('cors')
-require('dotenv').config()
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
 
-const contactRoutes = require('./routes/contactRoutes')
-const app = express()
+const contactRoutes = require("./routes/contactRoutes");
+const app = express();
 
-app.use(cors({
-    origin: ['http://localhost:3000', 'http://10.122.91.32:3000'],
-    methods: ['GET', 'POST'],
-    allowedHeaders: ['Content-Type']
-  }))
-app.use(express.json())
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000",
+      "http://10.122.91.32:3000",
+      "https://portfolio-frontend-beige-six.vercel.app",
+    ],
+    methods: ["GET", "POST"],
+    allowedHeaders: ["Content-Type"],
+  })
+);
+app.use(express.json());
 
-app.use('/api/contact',contactRoutes)
+app.use("/api/contact", contactRoutes);
 
-app.get('/',(req,res)=>{
-    res.json({message: 'Backend is running!'})
-})
+app.get("/", (req, res) => {
+  res.json({ message: "Backend is running!" });
+});
 
-const PORT = process.env.PORT || 5003
-app.listen(PORT,()=>{
-    console.log(`server running on port ${PORT}`)
-})
+const PORT = process.env.PORT || 5003;
+app.listen(PORT, () => {
+  console.log(`server running on port ${PORT}`);
+});
